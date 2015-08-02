8ca746d3a62c112125d76369945307195d8ef8f3
Ryan Jones <run0391@gmail.com>
2015-08-01 18:39:48 -0600

    fix migration files

--- db/migrate/20150801225936_add_location_table.rb
+++ db/migrate/20150801225936_add_location_table.rb
@@ -1,4 +1,4 @@
-class Location < ActiveRecord::Migration
+class AddLocationTable < ActiveRecord::Migration
   def change
     create_table :locations do |t|
       t.string :city
@@ -10,3 +10,8 @@ class Location < ActiveRecord::Migration
     end
   end
 end
+Location.create(:city => "Los Angeles", :country => "USA", :region => "North America", :continent => "North America")
+Location.create(:city => "Auckland", :country => "New Zealand", :region => "South Pacific", :continent => "Australia")
+la = Location.first
+nz = Location.last
+Flight.create(:originating_location_id => 1, :destination_location_id => 2, :price => 800, :available_months => "January February", :minimum_duration => 10, :link_to_deal => "http://nba.com", :posted_on => Date.current, :is_international => true)
